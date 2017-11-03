dfcfbae8c446147e4161735af91baedabc515783
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-11-02 22:17:53 -0400

    Add files via upload

---
+++ exercise2/pixel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python    
+# encoding: utf-8    
+  
+import numpy as np  
+import cv2  
+  
+baboon = cv2.imread("/Users/wangmengxi/Documents/mercy/ec601/openCV/OpenCV_homework/Test_images/baboon.jpg")
+hsv=cv2.cvtColor(baboon,cv2.COLOR_BGR2HSV)
+ycrcb=cv2.cvtColor(baboon,cv2.COLOR_BGR2YCrCb)
+(b,g,r) = baboon[20,25]
+(Cr,Cb,Y)=ycrcb[20,25]
+(v,s,h)=hsv[20,25]
+#print("baboon.jpg:")
+print("BGR:")
+
+print(b,g,r)
+
+print("ycrcb:")
+
+print(Cr,Cb,Y)
+
+print("hsv:")
+print(v,s,h)
+
+
